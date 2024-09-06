001b987f9aa9db72c1c6a3b89b595d16cc752682
VSTERNENLICHT <seoyoungp0311@gmail.com>
2024-09-07 04:46:13 +0800

    first commit

---
+++ runml.c
@@ -0,0 +1,20 @@
+//  CITS2002 Project 1 2024
+//  Student1:   23902644   Seoyoung Park
+//  Student2:   23915299   Harper Chen
+//  Platform:   Apple
+
+// Compile this program with:
+//      cc -std=c11 -Wall -Werror -o runml runml.c
+
+// Please add comments and check if it runs without any errors before you push !!!
+
+
+#include <stdio.h>	// prinf, fprintf, stderr, fopen
+#include <stdlib.h>	// exit, EXIT_SUCCESS, atoi
+#include <string.h>	// strlen
+#include <ctype.h>	// islower
+
+int main(int argcount, char *argvalue[])
+{
+
+}
