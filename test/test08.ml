ace84119f6ea85f8f9f5132513f4a95334a57a3d
陈夏怡 <harper@chenxiayideMacBook-Pro.local>
2024-09-10 13:41:37 +0800

    testfiles

---
+++ test/test08.ml
@@ -0,0 +1,15 @@
+function testone t e s t
+	return 1
+
+function testtwo (t, e, s, t)
+	return 2
+
+function testthree(t e s t)
+	return 3
+
+function testfour (t,e,s,t)
+	return 4
+
+print testone (1,2, 3 ,4) + testtwo (1, 2, 3, 4)
+print testone(1, 2, 3, 4) / testthree(1, 2,3,4)
+print testfour (1, 2,3, 4)*testtwo(1,2, 3, 4)
